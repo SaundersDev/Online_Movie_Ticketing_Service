e590418ca590197f00ca1605de8fa46acc3f030e
PatGibson <patrick.gibson1996@gmail.com>
2018-03-23 17:28:22 -0400

    review

--- MovieTickets/Customer/review.php
+++ MovieTickets/Customer/review.php
@@ -18,26 +18,66 @@ $conn =  mysqli_connect($servername, $username, $password, $dbname);
 <a href ="homepage.php">
 	<img src="images/home.png" alt="Home Page" style="width:50px;height:50px;"/>
 </a>
-
+<?php 
+	$movie = $_GET["title"];
+	$getMovie = "Select runningtime, rating, director, productioncompany, plotsynopsis from movie where title = '$movie'";
+	$resultMovie = mysqli_query($conn, $getMovie);
+	$row = mysqli_fetch_assoc($resultMovie);
+	//get picture for corresponding movie
+	$runningTime = $row["runningtime"];
+	$rating = $row["rating"];
+	$director = $row["director"];
+	$productionCompany = $row["productioncompany"];
+	$plotSynopsis = $row["plotsynopsis"];
+?>
 <h2>
 	Write a Review
 </h2>
-<form id="userReview" action='<?php echo $_SERVER["PHP_SELF"];?>' method = "post">
-	<input type= "textarea" width = "400" height = "250" maxlength="255">
-	<br><br>
-	<input type="submit" name="submit" value="Submit Review">
-</form>
-<div class="slidecontainer">
+<textarea maxlength="255" id="reviewBox" rows="4" cols="50" > 
+	
+</textarea>
+<div id="slidecontainer">
   <input type="range" min="0" max="5" value="2.5" class="slider" step="0.1" id="movieRating">
   <p>Rating: <span id="num"></span></p>
+  <button type="button" onclick="submitReview()">Submit Review</button> 
 </div>
-
+<div  id="movieDetails" >
+	<br>
+	<img src="images/<?php echo $movie;?>.jpg" alt="<?php echo $movie;?>" style="width:250px;height:350px;"/> 
+	<p> <strong>Plot Synopsis:</strong><?php echo ' '.$plotSynopsis.' <br>';?> 
+		<strong>Rating:</strong><?php echo ' '.$rating.' <br>';?>
+		<strong>Director:</strong><?php echo ' '.$director.' <br>';?>
+		<strong>Production Company:</strong><?php echo ' '.$productionCompany.' <br>';?>
+		<strong>Running Time:</strong><?php echo ' '.$runningTime.' minutes <br>'?>
+	</p>
+</div>
 <script>
 var slider = document.getElementById("movieRating");
 var output = document.getElementById("num");
 output.innerHTML = slider.value;
 slider.oninput = function() {
   output.innerHTML = this.value;
+}
+
+function submitReview(){
+
+	var mysql = require('mysql');
+	var con = mysql.createConnection({
+	  host: "localhost",
+	  user: "root",
+	  password: "",
+	  database: "MovieTix"
+	});
+
+	con.connect(function(err) {
+	  if (err) throw err;
+	  console.log("Connected!");
+	  var sql = "INSERT INTO reviews (text, score, reviewermovietitle, reviewername) VALUES ('hello', '5', 'Black Panther', 'pgibs')";
+	  con.query(sql, function (err, result) {
+	    if (err) throw err;
+	    console.log("1 record inserted");
+	  });
+	});
 }
 
 </script>
