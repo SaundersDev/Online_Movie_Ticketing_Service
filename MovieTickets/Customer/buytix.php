02b0d5216e50e1ac91d68a7bd00ad96799d13caa
PatGibson <patrick.gibson1996@gmail.com>
2018-03-25 16:21:36 -0400

    Reservations

--- MovieTickets/Customer/buytix.php
+++ MovieTickets/Customer/buytix.php
@@ -6,8 +6,7 @@ $password = "";
 $dbname = "MovieTix";
 $conn =  mysqli_connect($servername, $username, $password, $dbname);
 $user = $_SESSION["username"];
-//$showing = $_GET["showing"];
-$showing = 1;
+$showing = $_GET["showing"];
 $getDetails = "Select movietitle, starttime from theatercomplex_movie_showing inner join showing where showing.id = theatercomplex_movie_showing.showingid and showing.id = $showing";
 $result = mysqli_query($conn, $getDetails);
 $row = mysqli_fetch_assoc($result);
@@ -18,13 +17,13 @@ $time = $row["starttime"];
 
 <html >
 <h2> Confirm Reservation </h2>
-<img src="images/<?php echo ''.$movietitle.'.jpg';?>"  style="width:250px;height:350px;">
+<img src="images/<?php echo ''.$movietitle.'.jpg';?>"  style="width:250px;height:350px;" alt="<?php echo $movietitle ?>">
 
 <h3> <?php echo "$movietitle"; ?> </h3>
 at: <?php echo "$time" ?>
 
 <form method="post" action="processBuytix.php">
-	<input type="number" name="number">
+	<input type="number" name="number" min="0">
 	<input type="hidden" name="showing" value="<?php echo $showing ?>">
 	<input type="submit" name="submit" value="Buy Tickets">
 </form>
--- MovieTickets/Customer/processBuytix.php
+++ MovieTickets/Customer/processBuytix.php
@@ -1,3 +1,19 @@
 <?php
-echo $_POST["showing"];
+session_start(); 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "MovieTix";
+$conn =  mysqli_connect($servername, $username, $password, $dbname);
+
+$showing =  $_POST["showing"];
+$number = $_POST["number"];
+
+//$username = $_SESSION["username"];
+$username = 'pgibs';
+
+$reserve = "insert into reservations (numtixreserved, username, showingid) values ('<?php echo $number ?>', '<?php echo $username ?>', '<?php echo $showing ?>')" ;
+mysqli_query($conn, $sql);
+header('Location: homepage.php');
+
 ?>
